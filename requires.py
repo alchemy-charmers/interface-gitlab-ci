a63e267b3a5bd576f4a950c7d6925ad90f5ecb70
Chris Sanders <sanders.chris@gmail.com>
2019-12-18 09:01:39 -0600

    Fix received server retrevial

--- requires.py
+++ requires.py
@@ -8,12 +8,11 @@ class GitLabCIRegistrationError(Exception):
 class GitLabCiRequires(Endpoint):
     @when_any("endpoint.{endpoint_name}.joined", "endpoint.{endpoint_name}.changed")
     def check_ci_server(self):
-        for relation in self.relations:
-            server_uri = relation.all_joined_units.received("server_uri")
-            server_token = relation.all_joined_units.received("server_token")
-            if server_uri and server_token:
-                set_flag(self.expand_name("available"))
-                return (server_uri, server_token)
+        server_uri = self.all_joined_units.received.get("server_uri", None)
+        server_token = self.all_joined_units.received.get("server_token", None)
+        if server_uri and server_token:
+            set_flag(self.expand_name("available"))
+            return (server_uri, server_token)
         return None
 
     @when("endpoint.{endpoint_name}.departed")
